5ef04344c9a1ded70c48cc7a31879142c81a99b9
Elisei Danciu <elisei.danciu@senseisrl.it>
2019-03-11 18:21:49 +0100

    Updates

---
+++ precache-manifest.8e2d8f6ba5ab8b9a66b8708f895e123a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c54f20ba28da6c0c666",
+    "url": "/drum-machine/static/css/main.dd3b380c.chunk.css"
+  },
+  {
+    "revision": "1c54f20ba28da6c0c666",
+    "url": "/drum-machine/static/js/main.a4793291.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/drum-machine/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "4bc7c247a1fee7c236f3bff4ab9c3163",
+    "url": "/drum-machine/index.html"
+  }
+];
